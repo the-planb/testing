d91b88b475d9b282e7b9bc21c1f556f8c66df0b0
jmpantoja <jmpantoja@gmail.com>
2022-10-07 13:40:20 +0200

    feat: Types Interfaces
    Algunas interfaces útiles para envolver primitivos

---
+++ src/Type/FloatValue.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlanB\Type;
+
+interface FloatValue
+{
+    public function toFloat(): float;
+}
---
+++ src/Type/IntegerValue.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PlanB\Type;
+
+interface IntegerValue
+{
+    public function toInt(): int;
+}
