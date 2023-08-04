3379a4552c0f9522a052044549bd95d0ab85ec96
jmpantoja <jmpantoja@gmail.com>
2023-08-04 14:16:09 +0200

    test: is_of_the_type
    Refactorizo la función is_of_the_type para mejorar el coverage

--- src/Resources/functions/types.php
+++ src/Resources/functions/types.php
@@ -1,6 +1,6 @@
 <?php
 
-if (! function_exists('is_of_the_type')) {
+if (!function_exists('is_of_the_type')) {
     function is_of_the_type(mixed $value, string ...$types): bool
     {
         if (0 === count($types)) {
@@ -8,33 +8,34 @@ if (! function_exists('is_of_the_type')) {
         }
 
         $temp = false;
-        foreach ($types as $type) {
+        while (false === $temp && false != $type = current($types)) {
             $type = strtolower($type);
-            $temp = $temp
-                || match ($type) {
-                    'mixed' => true,
-                    'object' => is_object($value),
-                    'resource' => is_resource($value),
-                    'array' => is_array($value),
-                    'string' => is_string($value),
-                    'int' => is_int($value),
-                    'integer' => is_integer($value),
-                    'float' => is_float($value),
-                    'double' => is_double($value),
-                    'bool', 'boolean' => is_bool($value),
-                    'null' => is_null($value),
-                    'callable' => is_callable($value),
-                    'countable' => is_countable($value),
-                    'iterable' => is_iterable($value),
-                    default => is_a($value, $type)
-                };
+            $temp = match ($type) {
+                'mixed' => true,
+                'object' => is_object($value),
+                'resource' => is_resource($value),
+                'array' => is_array($value),
+                'string' => is_string($value),
+                'int' => is_int($value),
+                'integer' => is_integer($value),
+                'float' => is_float($value),
+                'double' => is_double($value),
+                'bool', 'boolean' => is_bool($value),
+                'null' => is_null($value),
+                'callable' => is_callable($value),
+                'countable' => is_countable($value),
+                'iterable' => is_iterable($value),
+                default => is_a($value, $type)
+            };
+
+            next($types);
         }
 
         return $temp;
     }
 }
 
-if (! function_exists('type_of')) {
+if (!function_exists('type_of')) {
     function type_of(mixed $value): string
     {
         $type = is_object($value) ? $value::class : gettype($value);
